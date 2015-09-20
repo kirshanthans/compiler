e8137fd6ab3113bb4ce37152e81ee89f13d779c9
kirshanthans <kirshanthans.14@cse.mrt.ac.lk>
2015-09-20 12:50:10 -0400

    Step3 test cases added

---
+++ step3/src/compiler.cpp
@@ -0,0 +1,16 @@
+#include "compiler.h"
+
+int main(int argc, char **argv){
+		Logger logger(4, "test.log"); 
+		
+		yyin = fopen(argv[1], "r");
+		
+		int parse = yyparse();
+		
+		if(parse)
+				cout << "Not accepted" << endl;
+		else
+				cout << "Accepted" << endl;
+
+		return 0; 
+}
---
+++ step3/src/compiler.h
@@ -0,0 +1,13 @@
+#ifndef _COMPILER
+#define _COMPILER
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "logger.h"
+#include "parser.h"
+#include "scanner.h"
+
+using namespace std; 
+
+#endif
